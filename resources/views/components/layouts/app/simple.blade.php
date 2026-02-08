8b6b554b23f0f804b08b03512f67a20515e22cc1
Paul Hibbert <30866933+paulhibbert@users.noreply.github.com>
2026-02-08 08:14:02 +0000

    add some basic styling for the pre element that commonmark renders fenced code with

--- resources/views/components/layouts/app/simple.blade.php
+++ resources/views/components/layouts/app/simple.blade.php
@@ -2,6 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
+        <style>
+            pre {
+                margin-top: 1em;
+                margin-bottom: 1em;
+                background: aliceblue;
+                border-radius: 0.5em;
+                padding: 0.5em;
+            }
+        </style>
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
