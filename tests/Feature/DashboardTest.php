3b66e77421f643d1169ed73a46e683ff2a15910f
Paul Hibbert <30866933+paulhibbert@users.noreply.github.com>
2026-02-08 08:34:55 +0000

    fix failing tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Models\User;
-
-test('guests are redirected to the login page', function () {
-    $this->get('/dashboard')->assertRedirect('/login');
-});
-
-test('authenticated users can visit the dashboard', function () {
-    $this->actingAs($user = User::factory()->create());
+test('guests can visit the dashboard', function () {
 
     $this->get('/dashboard')->assertStatus(200);
 });
